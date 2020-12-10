c525939241936d0a527a5e58c62a573fe4000e5d
ultram4rine <ultramarine730@gmail.com>
2020-12-10 16:59:40 +0400

    button component

---
+++ ui/src/Button.svelte
@@ -0,0 +1,113 @@
+<script>
+  import Icon from "svelte-awesome";
+  import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+
+  let on = false;
+</script>
+
+<style>
+  section {
+    margin: 150px auto 0;
+    width: 75px;
+    height: 95px;
+    position: relative;
+    text-align: center;
+  }
+  :active,
+  :focus {
+    outline: 0;
+  }
+
+  #button {
+    font-family: "FontAwesome";
+    color: rgb(37, 37, 37);
+    text-shadow: 0px 1px 1px rgba(250, 250, 250, 0.1);
+    font-size: 32pt;
+    display: block;
+    position: relative;
+    text-decoration: none;
+    background-color: rgb(83, 87, 93);
+    box-shadow: 0px 3px 0px 0px rgb(34, 34, 34),
+      /* 1st Shadow */ 0px 7px 10px 0px rgb(17, 17, 17),
+      /* 1nd Shadow */ inset 0px 1px 1px 0px rgba(250, 250, 250, 0.2),
+      /* 3rd Shadow */ inset 0px -12px 35px 0px rgba(0, 0, 0, 0.5); /* 4th Shadow */
+    width: 70px;
+    height: 70px;
+    border: 0;
+    border-radius: 35px;
+    text-align: center;
+    line-height: 79px;
+    transition: color 350ms, text-shadow 350ms;
+    -o-transition: color 350ms, text-shadow 350ms;
+    -moz-transition: color 350ms, text-shadow 350ms;
+    -webkit-transition: color 350ms, text-shadow 350ms;
+  }
+  #button:before {
+    content: "";
+    width: 80px;
+    height: 80px;
+    display: block;
+    z-index: -2;
+    position: absolute;
+    background-color: rgb(26, 27, 29);
+    left: -5px;
+    top: -2px;
+    border-radius: 40px;
+    box-shadow: 0px 1px 0px 0px rgba(250, 250, 250, 0.1),
+      inset 0px 1px 2px rgba(0, 0, 0, 0.5);
+  }
+  #button.on {
+    box-shadow: 0px 0px 0px 0px rgb(34, 34, 34), 0px 3px 7px 0px rgb(17, 17, 17),
+      inset 0px 1px 1px 0px rgba(250, 250, 250, 0.2),
+      inset 0px -10px 35px 5px rgba(0, 0, 0, 0.5);
+    background-color: rgb(83, 87, 93);
+    top: 3px;
+    color: #fff;
+    text-shadow: 0px 0px 3px rgb(250, 250, 250);
+  }
+  #button:active {
+    box-shadow: 0px 0px 0px 0px rgb(34, 34, 34),
+      /* 1st Shadow */ 0px 3px 7px 0px rgb(17, 17, 17),
+      /* 2nd Shadow */ inset 0px 1px 1px 0px rgba(250, 250, 250, 0.2),
+      /* 3rd Shadow */ inset 0px -10px 35px 5px rgba(0, 0, 0, 0.5); /* 4th Shadow */
+    background-color: rgb(83, 87, 93);
+    top: 3px;
+  }
+  #button:active:before,
+  #button.on:before {
+    top: -5px;
+    background-color: rgb(26, 27, 29);
+    box-shadow: 0px 1px 0px 0px rgba(250, 250, 250, 0.1),
+      inset 0px 1px 2px rgba(0, 0, 0, 0.5);
+  }
+
+  #button + span {
+    display: block;
+    width: 8px;
+    height: 8px;
+    background-color: rgb(226, 0, 0);
+    box-shadow: inset 0px 1px 0px 0px rgba(250, 250, 250, 0.5),
+      0px 0px 3px 2px rgba(226, 0, 0, 0.5);
+    border-radius: 4px;
+    clear: both;
+    position: absolute;
+    bottom: 0;
+    left: 42%;
+  }
+  #button.on + span {
+    box-shadow: inset 0px 1px 0px 0px rgba(250, 250, 250, 0.5),
+      0px 0px 3px 2px rgba(135, 187, 83, 0.5);
+    background-color: rgb(135, 187, 83);
+    transition: background-color 350ms, box-shadow 700ms;
+    -o-transition: background-color 350ms, box-shadow 700ms;
+    -moz-transition: background-color 350ms, box-shadow 700ms;
+    -webkit-transition: background-color 350ms, box-shadow 700ms;
+  }
+</style>
+
+<section>
+  <div id="button" class:on on:click={() => (on = !on)}>
+    <Icon data={faPowerOff} scale="2" />
+  </div>
+  <span />
+</section>
