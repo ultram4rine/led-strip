cdf751faf2ab779f129b859dee8ca7e86a66728f
ultram4rine <ultramarine730@gmail.com>
2020-12-10 15:33:09 +0400

    hello warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(warnings)]
+mod controller;
+mod led;
+
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let routes = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
