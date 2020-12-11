a52e1ff3b77aea091be0b78b5cbd7ffe2cbab4d6
ultram4rine <ultramarine730@gmail.com>
2020-12-12 00:05:41 +0400

    fixed routes usage

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod api;
 mod controller;
 mod led;
 
-use crate::api::api::color_routes;
-
 #[tokio::main]
 async fn main() {
-    warp::serve(color_routes).run(([127, 0, 0, 1], 3030)).await;
+    let routes = api::api::color_routes();
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
