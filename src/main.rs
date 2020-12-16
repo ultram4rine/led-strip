4fc997618d65b1e03a38831b227f1867dc5662fa
ultram4rine <ultramarine730@gmail.com>
2020-12-16 16:24:19 +0400

    some shit

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 #![deny(warnings)]
-mod api;
+#![allow(dead_code)]
 mod controller;
 mod led;
 
+extern crate linux_embedded_hal as hal;
+extern crate pwm_pca9685 as pca9685;
+
+use crate::controller::controller::Controller;
+use crate::led::led::LED;
+use std::convert::Infallible;
+use warp::{http, Filter};
+
 #[tokio::main]
 async fn main() {
-    let routes = api::api::color_routes();
+    let controller = Controller::new();
+
+    let routes = color(controller.clone()).with(warp::cors().allow_any_origin());
 
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn color(
+    controller: Controller,
+) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::path!("color")
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(warp::any().map(move || controller.clone()))
+        .and_then(apply_color)
+}
+
+async fn apply_color(led: LED, mut controller: Controller) -> Result<impl warp::Reply, Infallible> {
+    controller.apply(led).await;
+    Ok(http::StatusCode::OK)
 }
